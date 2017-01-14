d2d1fa9db3e758d379d4fcf7b814abed8ecc214b
Flowlishes <florianwolf1@Nakama-4.local>
2017-01-14 12:38:29 +0100

    Update
    Working Chat

--- client.py
+++ client.py
@@ -1,17 +1,37 @@
+"""!/usr/bin/python
+-*- coding: UTF-8 -*-
+Date: 14.01.2017
+"""
 import socket
-ip = input("IP-Adresse: ")
 
+#Eingabe der IP-Adresse
+ip = input("IP-Adresse des Zielservers: ")
+
+#Aufbau des Sockets
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#Verbindung mit dem Server
 s.connect((ip, 50000))
-name = input("Dein Name: ")
-name = name + ": "
+name = str(input("Dein Name: ")) + ": "
+
+#Abbruchbedingung
+quitClient = False
+
 try:
-    while True:
-        message = input("Antwort: ")
-        message = name + str(message)
-        s.send(message.encode())
+    while quitClient == False:
+        nachricht = input("Nachricht: ")
+        if nachricht == "quit()":                   #Abbruch der Kommunikation-->finally
+            quitClient = True
+            break
+        nachricht = name + nachricht +"\n"
+        s.send(nachricht.encode())
         antwort = s.recv(1024)
-        print("[{}]{}".format(ip, antwort.decode()))
-finally:
+        print(antwort.decode())
+finally:                                            #Trennen der Verbdingung
     s.close()
-    print("Client: Server closed.\n")
+    print("""\
+\n
+-------------------------------
+Verbindung zum Server getrennt.
+-------------------------------
+""")
--- server.py
+++ server.py
@@ -1,61 +1,32 @@
 """!/usr/bin/python
 -*- coding: UTF-8 -*-
-Date: 11.01.2017
+Date: 14.01.2017
 """
 import socket
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)					#Socket des Moduls Socket wird erstellt
-s.bind(("", 50000))														#.bind benötigt Tupel als Eingabe
-s.listen(1)																#Maximale Anzahl an Verbindungspartnern
 
-quit = False															#Abbruchbedingung
-s.setblocking(False)													#Socket soll nicht blockieren --> Kommunikation möglich
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind(("", 50000))
+s.listen(1)
 
-name = input("Dein Name: ")												#Der Server wählt einen Chatnamen
-name = name + ": "
 
-while quit == False:
-	try:
-		while True:
-		#Verbidnungssocket
-			komm, addr = s.accept()										#s.accpet() gibt Tupel mit connection und adresse zurück
-			while True:
-			#Kommunikationssocket
-				data = komm.recv(1024)									#Daten werden empfangen --> Wert in ()?
-				if data == "help()":
-				    #Abstände checken.
-					#nicht print, sondern send
-				    print("""\
-				    -----------------------------------------------------
-				    Help menu:
-				    -----------------------------------------------------
-				    Commands for the chat:
-				        quit() for quitting the chat
-				        name() for the name of your communication partner
-				        address() for the address of your partner
-				    -----------------------------------------------------
-				    """)
-				elif data == "quit()":									#Beenden der Kommunikation
-					quit = True											#evtl durch break ersetzen
-					komm.close()
-				elif data == "name()":									#Rückgabe gewählter Name des Partner (Server), nicht print() sondern send()
-				    #komm.send(name.encode())							#?
-				    pass
-				elif data == "address":									#Rückgabe des verwendeten Ports, der IP-Adresse und des Hosts
-				    #komm.send(addr.encode())							#?
-				    pass
-				elif data == "":
-					pass
-				print("[{}]{}".format(addr[0], data.decode()))			#Ausgabe der Nachricht
-				message = name + str(input("Antwort: "))				#Typ des Rückgabewerts testen.
-				komm.send(message.encode())
-	except:
-		pass
-#finally:
-s.close()																#Schließen des Verbindungssockets
-print("Server: Server closed.")
+name = str(input("Dein Name: ")) + ": "
 
-#https://docs.python.org/3/library/socket.html#socket.socket.accept
-#http://wwwlehre.dhbw-stuttgart.de/~schepper/Vorlesung04.pdf
+quitServer = False
 
-# wenn data "" ist
+try:
+    while quitServer == False:
+        #Verbdingungssocket
+        komm, addr = s.accept()
+        while quitServer == False:
+            #Kommunikationssocket
+            data = komm.recv(1024)
+            print(data.decode())
+            nachricht = name + str(input("Antwort: ")) + "\n"
+            if nachricht == "quit()":
+                quitServer = True
+                break
+            komm.send(nachricht.encode())
+finally:
+    s.close()
+    print("Server wird geschlossen...")
