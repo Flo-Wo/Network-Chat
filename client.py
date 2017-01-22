05acc5cada2b4f28e5087ada3d046cd3c9799d87
Flowlishes <florianwolf1@Nakama-4.local>
2017-01-22 17:06:09 +0100

    Verbesserte Abbruchbedingungen
    Server zeigt nun auf Anfrage eigene IP an und Client/Server können
    besser abbrechen

--- client.py
+++ client.py
@@ -5,47 +5,57 @@ Date: 14.01.2017
 import socket
 import os
 
-#Eingabe der IP-Adresse, entweder lokal Ethernet Verbindungen oder über eine Eingabe
-Eingabe = input("Verbindung zu einem Rechner im lokalen Netzwerk aufbauen (Ja/Nein): ")
+# Eingabe der IP-Adresse, entweder lokal Ethernet Verbindungen oder über eine Eingabe
+eingabe = input("Verbindung zu einem Rechner im lokalen Netzwerk aufbauen (ja/nein): ")
 
-if Eingabe == "Ja":
+if eingabe == "ja":
     print("Liste aller über Ethernet verbunden Geräte:\n\n")
     rechner = os.popen("arp -a").readlines()
-    for i in range (0, len(rechner)):           #braucht nicht -1, range immer < als Angabe ist
-        nummer = str(i) + ": "                  #Nummer vor Listenobjekt
+    for i in range(0, len(rechner)):                # braucht nicht -1, range immer < als Angabe ist
+        nummer = str(i) + ": "                      # Nummer vor Listenobjekt
         print(nummer + rechner[i])
-    index = int(input("wähle deine Ziel-IP aus (Nummer): "))
-    rechnerneu = rechner[index].split(" ")      #String --> Liste
-    rechnerwert = rechnerneu[1]                 #Index der IP in der Liste
-    rechner1 = rechnerwert.replace("(", "")
-    ip = rechner1.replace(")", "")   #Entfernen der Klammern--> IP
-    #print(ip)
-elif Eingabe == "Nein":
+    index = input("Wähle deine Ziel-IP aus (Nummer, oder 'nicht in der Liste'): ")
+    try:
+        index = int(index)
+        rechnerneu = rechner[index].split(" ")          # String --> Liste
+        rechnerwert = rechnerneu[1]                     # Index der IP in der Liste
+        rechner1 = rechnerwert.replace("(", "")
+        ip = rechner1.replace(")", "")
+        print("Verbindung zum Server wird aufgebaut...\n")# Entfernen der Klammern--> IP
+        # print(ip)
+    except ValueError:                                  #Wenn es nicht in der Liste ist
+        if index == "nicht in der Liste":
+            ip = input("IP-Adresse des Zielservers: ")
+            print("Verbindung zum Server wird aufgebaut...\n")
+        else:
+            print("Ungültie Eingabe.")
+elif eingabe == "nein":
     ip = input("IP-Adresse des Zielservers: ")
+    print("Verbindung zum Server wird aufgebaut...\n")
 else:
     print("Ungültige Eingabe.")
 
-#Aufbau des Sockets
+# Aufbau des Sockets
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-#Verbindung mit dem Server
+# Verbindung mit dem Server
 s.connect((ip, 50000))
 name = str(input("Dein Name: ")) + ": "
 
-#Abbruchbedingung
+# Abbruchbedingung
 quitClient = False
 
 try:
     while quitClient == False:
         nachricht = input("Nachricht: ")
-        if nachricht == "quit()":                   #Abbruch der Kommunikation-->finally
+        if nachricht == "quit()":                     # Abbruch der Kommunikation-->finally
             quitClient = True
             break
-        nachricht = name + nachricht +"\n"
+        nachricht = name + nachricht + "\n"
         s.send(nachricht.encode())
         antwort = s.recv(1024)
         print(antwort.decode())
-finally:                                            #Trennen der Verbdingung
+finally:                                               # Trennen der Verbindung
     s.close()
     print("""\
 \n
--- server.py
+++ server.py
@@ -3,12 +3,22 @@
 Date: 14.01.2017
 """
 import socket
-
+import os
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(("", 50000))
 s.listen(1)
 
+e = input("Eigene IP-Adresse anzeigen? (ja/nein) ")
+if e == "ja":
+    alleips = os.popen("ifconfig |grep inet").readlines()
+    alleips = alleips[4].split(" ")
+    print("Deine IP-Adresse lautet: " + alleips[1])
+    print("Server startet...\n")
+elif e == "nein":
+    print("Server startet...\n")
+else:
+    print("Ungültige Eingabe.\nServer startet...\n")
 
 name = str(input("Dein Name: ")) + ": "
 
@@ -22,11 +32,21 @@ try:
             #Kommunikationssocket
             data = komm.recv(1024)
             print(data.decode())
-            nachricht = name + str(input("Antwort: ")) + "\n"
+            nachricht = str(input("Antwort: "))
             if nachricht == "quit()":
                 quitServer = True
+                nachricht = "\nDer Server hat die Kommunikation beendet.\n"
+                komm.send(nachricht.encode())
                 break
-            komm.send(nachricht.encode())
+            else:
+                #Konvertierung, Name dazu
+                nachricht = name + nachricht + "\n"
+                komm.send(nachricht.encode())
 finally:
     s.close()
-    print("Server wird geschlossen...")
+    print("""\
+\n
+----------------------------------------------------
+Verbindung zum Client getrennt, Server wird beendet.
+----------------------------------------------------
+""")
